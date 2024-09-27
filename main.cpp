04489bf283625fddf9cf7487faafde73c00cf753
Lebohang <mmokhwibitjane023b@student.wethinkcode.co.za>
2024-09-27 14:57:42 +0200

    Basic window up and running

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <raylib.h>
 
 int main(int argno, char** args){
-  std::cout<<"Hello world"<<std::endl;
+  const int screen_width = 1024;
+  const int screen_hieght = 768;
+  InitWindow(screen_width, screen_hieght, "Synth");
+  SetTargetFPS(60);
+
+  while(WindowShouldClose() == false){
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("Synth", 100, 100, 20, RED);
+    EndDrawing();
+  }
+  CloseWindow();
   return 0;
 }
